9ea4fa80c0e8040492916d70db916efcd1cbc963
Brendan Mackie <brendan.mackie@gmail.com>
2015-07-30 15:51:35 -0400

    Started the process of making Acts as Taggable to work

--- app/helpers/daily_entries_helper.rb
+++ app/helpers/daily_entries_helper.rb
@@ -1,2 +1,3 @@
 module DailyEntriesHelper
+	include ActsAsTaggableOn::TagsHelper
 end
--- app/models/daily_entry.rb
+++ app/models/daily_entry.rb
@@ -1,2 +1,4 @@
 class DailyEntry < ActiveRecord::Base
+  acts_as_taggable # Alias for acts_as_taggable_on :tags
+  acts_as_taggable_on :mood, :activity
 end
